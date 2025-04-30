9589d35399ed26c5ce0feeeca809b0677ce92566
niel2512 <nielyusuf17@gmail.com>
2025-05-01 00:21:44 +0700

    Create Filament CRUD Resource BookingTransaction

--- app/Filament/Resources/BookingTransactionResource.php
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\BookingTransactionResource\Pages;
-use App\Filament\Resources\BookingTransactionResource\RelationManagers;
-use App\Models\BookingTransaction;
+use Dom\Text;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use App\Models\BookingTransaction;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\BookingTransactionResource\Pages;
+use App\Filament\Resources\BookingTransactionResource\RelationManagers;
 
 class BookingTransactionResource extends Resource
 {
@@ -23,7 +29,46 @@ class BookingTransactionResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                ->required()
+                ->maxlength(255),
+
+                TextInput::make('booking_trx_id')
+                ->required()
+                ->maxlength(255),
+
+                TextInput::make('phone_number')
+                ->required()
+                ->maxlength(13),
+
+                TextInput::make('total_amount')
+                ->required()
+                ->numeric()
+                ->prefix('Rp. '),
+
+                TextInput::make('duration')
+                ->required()
+                ->numeric()
+                ->prefix('Hari'),
+
+                DatePicker::make('started_at')
+                ->required(),
+
+                DatePicker::make('ended_at')
+                ->required(),
+
+                Select::make('is_paid')
+                ->options([
+                    true => 'Sudah Bayar',
+                    false => 'Belum Bayar',
+                ])
+                ->required(),
+
+                Select::make('office_space_id')
+                ->relationship('officeSpace', 'name') // Mengambil data dari model officeSpace pada field nama
+                ->required()
+                ->searchable()
+                ->preload(),
             ]);
     }
 
@@ -31,7 +76,25 @@ class BookingTransactionResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('booking_trx_id')
+                ->searchable(),
+                TextColumn::make('name')
+                ->searchable(),
+
+                TextColumn::make('officeSpace.name'),
+
+                TextColumn::make('started_at')
+                ->date(),
+                TextColumn::make('ended_at')
+                ->date(),
+
+                IconColumn::make('is_paid')
+                ->boolean()
+                ->trueColor('success')
+                ->falseColor('danger')
+                ->trueIcon('heroicon-o-check-circle')
+                ->falseIcon('heroicon-o-x-circle')
+                ->label('Sudah Bayar?'),
             ])
             ->filters([
                 //
