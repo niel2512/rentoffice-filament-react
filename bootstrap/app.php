d3a62ddd7b8840e4b68d4cc4048d4e68281493af
niel2512 <nielyusuf17@gmail.com>
2025-05-01 23:17:41 +0700

    setting api key

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Register the middleware for API key checking
+        // This middleware checks if the API key is present in the request header
+        // and if it is valid by checking against the database
+        // If the API key is not present or invalid, it returns a 401 Unauthorized response
+        $middleware->alias([
+            'api_key' => App\Http\Middleware\CheckApiKey::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -10,18 +10,21 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-//method get hanya untuk mengambil data    
-//tapi kita bisa juga menggunakan method get untuk mengambil data tertentu
-Route::get('/city/{city:slug}', [CityController::class, 'show']);
-
-//method resource bisa dipakai untuk CRUD tanpa perlu get 
-//cukup resource sebenernya sudah cukup untuk semuanya
-Route::apiResource('/cities', CityController::class); 
-
-Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
-Route::apiResource('/offices', OfficeSpaceController::class);
+//middleware apikey untuk memeriksa apakah api key valid
+Route::middleware('api_key')->group(function(){  //api-key diambil dari app.php
     
-Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);//method post untuk menyimpan data
-Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']); 
+    //method get hanya untuk mengambil data    
+    //tapi kita bisa juga menggunakan method get untuk mengambil data tertentu
+    Route::get('/city/{city:slug}', [CityController::class, 'show']);
 
+    //method resource bisa dipakai untuk CRUD tanpa perlu get 
+    //cukup resource sebenernya sudah cukup untuk semuanya
+    Route::apiResource('/cities', CityController::class); 
+
+    Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
+    Route::apiResource('/offices', OfficeSpaceController::class);
+    
+    Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);//method post untuk menyimpan data
+    Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']); 
+
+});
