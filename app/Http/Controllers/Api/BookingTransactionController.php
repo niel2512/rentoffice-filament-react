b54bbbe41921874fa5fae745a16e5e59541a52b2
niel2512 <nielyusuf17@gmail.com>
2025-05-03 17:05:28 +0700

    Add API BookingTransaction

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
+use App\Models\BookingTransaction;
+use App\Models\OfficeSpace;
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
-    //
+    public function store(StoreBookingTransactionRequest $request)
+    {
+        //validasi data
+        $validatedData = $request->validated();
+
+        $officeSpace = OfficeSpace::find($validatedData['office_space_id']);
+
+        $validatedData['is_paid'] = false;
+        $validatedData['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
+        $validatedData['duration'] = $officeSpace->duration;
+
+        $validatedData['ended_at'] = (new \DateTime($validatedData['started_at']))->modify("+{$officeSpace->duration} days")->format('Y-m-d');
+
+        //simpan data ke database
+        $bookingTransaction = BookingTransaction::create($validatedData);
+    }
 }
